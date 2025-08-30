eeee0ced6d2b8fed764d4e65e151723edd1e4121
vinicius-11340 <vinicius_zakalusny@estudante.sesisenai.org.br>
2025-08-29 21:48:55 -0300

    .

--- exercicio6.php
+++ exercicio6.php
@@ -1,36 +1,41 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício  - </title>
+    <title>Exercício 6 - Divisores </title>
 </head>
 
 
 <body>
 
-    <h1>Exercício  - </h1>
+    <h1>Exercício 6 - Divisores </h1>
     <form method="post">
         <label for="numero">Digite o primeiro número:</label>
         <input type="number" id="numero_1" name="numero_1" required>
         <br>
-        <button type="submit" name=" "></button>
+        <button type="submit" name="verificar">Verificar</button>
     </form>
 
-     <?php
+    <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['verificar'])) {
             $numero_1 = filter_var($_POST['numero_1'], FILTER_VALIDATE_INT);
             if ($numero_1 === false) {
                 echo "Número inválido!";
-                
             } else {
-               
-                
+                 echo "Os divisores de $numero_1 são: <br>" ;                 
+                for ($i = 1; $i <= $numero_1; $i++) {
+                    if ($numero_1 % $i == 0) {
+                        echo "$i <br>" ;
+                    }
+                }
             }
         }
     }
-    ?>  
+    ?>
 
 </body>
+
 </html>
--- exercicio7.php
+++ exercicio7.php
@@ -1,37 +1,60 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício  - </title>
+    <title>Exercício 7 - Número Perfeito </title>
 </head>
 
 
 <body>
 
-    <h1>Exercício  - </h1>
+    <h1>Exercício 7 - Número Perfeito </h1>
     <form method="post">
         <label for="numero">Digite o primeiro número:</label>
         <input type="number" id="numero_1" name="numero_1" required>
         <br>
-        <button type="submit" name=" "></button>
+        <button type="submit" name="verificar">Verificar</button>
     </form>
 
-     <?php
+
+
+    <?php
+
+    echo "<br>";
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['verificar'])) {
             $numero_1 = filter_var($_POST['numero_1'], FILTER_VALIDATE_INT);
-            if ($numero_1 === false) {
-                echo "Número inválido!";
-                
+            if ($numero_1 === false || $numero_1 <= 0) {
+                echo "Por favor, digite um número inteiro positivo!";
             } else {
-                for ($i = 1; $i <= 10; $i++) {
-             echo "$numero_1 x $i =" . ($numero_1 * $i) . "<br>"; 
+                $soma_divisores = 0;
+                echo "Os divisores de $numero_1 são: <br>";
+                for ($i = 1; $i < $numero_1; $i++) {
+                    if ($numero_1 % $i == 0) {
+                        echo "$i <br>";
+                        $soma_divisores += $i;
+                    }
                 }
+            }
+
+    echo "<br>";
+
+            if ($soma_divisores == $numero_1) {
+                echo "$numero_1 <strong>é um número perfeito!</strong> ";
+                echo "<br>Soma dos divisores próprios (exceto $numero_1) é igual a: $soma_divisores é igual a ele mesmo.";
+            } else {
+                echo "$numero_1 <strong>não é um número perfeito!</strong>";
+                echo "<br>Soma dos divisores próprios (exceto $numero_1) é igual a: $soma_divisores é diferente dele mesmo.";
             }
         }
     }
-    ?>  
+    ?>
 
 </body>
+
+
+
 </html>
